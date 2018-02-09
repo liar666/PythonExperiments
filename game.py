6717fb75dca4b3d57559c190a9deb7520fce3153
Guillaume MULLER <3216927+liar666@users.noreply.github.com>
2018-02-09 19:45:56 +0100

    drawing terrain almost OK with 1 pixel linewidth (remains <0 values ????)

--- game.py
+++ game.py
@@ -12,9 +12,11 @@ except ImportError:
     import tkinter  ## notice lowercase 't' in tkinter here
 
 # Set useful constants
-WINDOW_WIDTH=200
+PIXEL_SIZE=4 # multiply all the drawings by this value
+WINDOW_WIDTH=100
 WINDOW_HEIGHT=100
-TRACK_SIZE=50
+TRACK_SIZE=WINDOW_WIDTH/4
+MOBILITY=TRACK_SIZE/6
 TERRAIN_FG_COLOR="black"
 TERRAIN_BG_COLOR="white"
 SPEED=60
@@ -22,22 +24,19 @@ SPEED=60
 # Create the GUI
 # - create window
 root_window = tkinter.Tk()
-root.configure(background=BG_COLOR)
+root_window.configure(background=TERRAIN_BG_COLOR)
 # - add canvas
 canvas = tkinter.Canvas(root_window, width=500, height=500)
-# - add events for left/right keypress
-canvas.bind_all("<KeyPress-Left>", move_left)
-canvas.bind_all("<KeyPress-Right>", move_right)
 canvas.pack()
 
-# terrain = None
-# last_center = None
+terrain = None
+last_center = None
 player_pos = None
 timer = None
 # TODO: make game go faster with time!
 
 # Quits the game (called on click on "Quit" button)
-def quitf:
+def quitf():
     timer.stop()
     root_window.destroy()
 
@@ -48,10 +47,11 @@ quit_button.pack()
 
 # Resets the game parameters (called on click on "Reset" button)
 def start_game():
-    time.sleep(20);
+    global terrain, last_center
     terrain, last_center = init_terrain()
     player_pos = last_center
     score = 0
+    sleep(20);
     update_terrain(terrain, last_center, player_pos, score)
 
 # - add start_game button to the GUI
@@ -60,53 +60,70 @@ reset_button = tkinter.Button(root_window, text="Start/Reset",
 reset_button.pack()
 
 # Moves player to the left
-def move_left():
-    player_pos--
-    if check_player_dead():
+## TODO: optimize: make a single function that tests key pressed and acts accordingly
+def move_left(event):
+    print(event)
+    global player_pos
+    player_pos = player_pos-1
+    if check_player_dead(terrain, player_pos):
         timer.stop()
     else:
         canvas.delete(canvas.find_withtag("player"))
         draw_player(player_pos)
 
+# - add event callback for left keypress
+canvas.bind_all("<KeyPress-Left>", move_left)
+
 # Moves player to the right
-def move_right():
-    player_pos++
-    if check_player_dead():
+## TODO: optimize: make a single function that tests key pressed and acts accordingly
+def move_right(event):
+    print(event)
+    global player_pos
+    player_pos = player_pos+1
+    if check_player_dead(terrain, player_pos):
         timer.stop()
     else:
         canvas.delete(canvas.find_withtag("player"))
         draw_player(player_pos)
+
+# - add callback for right keypress
+canvas.bind_all("<KeyPress-Right>", move_right)
 
 # Create a new line for the terrain
 def compute_line(last_center):
-    new_center = last_center+randint(-TRACK_SIZE/4, TRACK_SIZE/4) # TODO: optimize
+    new_center = last_center+randint(-MOBILITY, MOBILITY) # TODO: optimize
+    if (new_center<1): new_center=1
+    if (new_center>(WINDOW_WIDTH-1)): new_center=WINDOW_WIDTH-1
     new_track_width = randint(TRACK_SIZE/2, TRACK_SIZE)
     return(new_center, new_track_width)
 
 # Initializes the terrain
 ## Optimization: we store only the "centers" and "track_width" for each line
 def init_terrain():
+    global player_pos
     terrain = []
     last_center = WINDOW_WIDTH/2
-    for l in range(WINDOW_HEIGHT):
+    for l in range(0, WINDOW_HEIGHT):
         new_center, new_track_width = compute_line(last_center)
         terrain += [ (new_center, new_track_width) ]
-        last_center = current_center
+        last_center = new_center
+    player_pos = last_center
     return(terrain, last_center)
 
 # Draws the terrain on the window canvas
+## Here use PIXEL_SIZE to zoom in
 def draw_terrain(terrain):
     for l in range(0,len(terrain)):
         curr_center, curr_width = terrain[l]
         l1 = canvas.create_line(0, l, curr_center-curr_width/2, l)
         l2 = canvas.create_line(curr_center+curr_width/2, l, canvas.winfo_width(), l)
-        itemconfig(l1, tags=("terrain"))
-        itemconfig(l2, tags=("terrain"))
+        canvas.itemconfig(l1, tags=("terrain"))
+        canvas.itemconfig(l2, tags=("terrain"))
 
 # Draws the player
 def draw_player(player_pos):
     pl = canvas.create_line(player_pos-1, len(terrain)-1, player_pos+1, len(terrain)-1)
-    itemconfig(pl, tags=("player"))
+    canvas.itemconfig(pl, tags=("player"))
 
 # Draws the whole game set
 def draw_game(terrain, player_pos, score):
@@ -119,9 +136,12 @@ def draw_game(terrain, player_pos, score):
 # - in fact delete most upwards line + create new line at bottom & redraws the whole terrain
 # TODO: optimize using canvas.move on all gfx objects tagged as  "terrain"?
 def update_terrain(curr_terrain, last_center, player_pos, score):
+    global terrain, last_center
     del(curr_terrain[0])
     new_center, new_track_width = compute_line(last_center)
     curr_terrain = curr_terrain + [(new_center, new_track_width)]
+    terrain = curr_terrain
+    last_center=new_center
     if check_player_dead():
         timer.stop()
     else:
@@ -132,7 +152,7 @@ def update_terrain(curr_terrain, last_center, player_pos, score):
 # Check if player is dead?
 def check_player_dead(terrain, player_pos):
     last_line=terrain[len(terrain)-1]
-    if (last_line[player_pos-1]==1 || last_line[player_pos+1]==1):
+    if (last_line[player_pos-1]==1 and last_line[player_pos+1]==1):
         return true
     else:
         return false
